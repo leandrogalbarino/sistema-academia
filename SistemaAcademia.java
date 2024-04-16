aca1c803dc25bf642ceff2e84a25641704aa17cd
Leandro Kise <leandrokise753@gmail.com>
2024-04-16 12:30:44 -0300

    Create SistemaAcademia.java

---
+++ SistemaAcademia.java
@@ -0,0 +1,18 @@
+//0. Os dados de alunos e de exercícios devem ser armazenados em uma base de dados relacional (postgres, mysql, sqlserver etc.). Os dados de outras entidades podem ser armazenados em listas em memória (quem desejar, pode armazená-los no banco também). - LEANDRO E LUCAS
+
+//1.Deve ser possível cadastrar alunos: incluir, alterar, excluir, listar, buscar pelo CPF, e pelo nome. Cada aluno deve ter: CPF, nome, data de nascimento. Leandro 
+
+//2.Deve ser possível cadastrar planos. Cada plano deve ter: código, nome, valor mensal. Lucas
+
+//3.Deve ser possível cadastrar exercícios. Cada exercício deve conter: número, nome, músculos ativados. Lucas
+
+//4.Para alunos cadastrados, deve ser possível ao instrutor: Leandro
+
+
+//5.Deve ser possível ao aluno, em determinada data, iniciar um treino: Leandro
+
+//6.Relatórios - Lucas
+
+
+
+
